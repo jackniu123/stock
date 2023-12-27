903d453c217114ab388da1c6028242462fd96605
jackniu123 <358137072@qq.com>
2023-12-27 12:57:04 +0800

    初始提交：已经完成：1,遍历所有股票，输出尾盘追涨次日卖出的盈利情况。

--- mysql.py
+++ mysql.py
@@ -21,77 +21,98 @@ try:
     print(result.fetchall())
     print('-------------------')
 
-    result = conn.execute(text('select * from daily where ts_code=\'000001.SZ\' limit 10000'))
-    print('========== select * from daily result:==========')
-    all_data = result.fetchall()
-    print(all_data)
+    result = conn.execute(text('select distinct ts_code from daily limit 10000'))
+    print('========== select distinct ts_code from daily limit 10000:==========')
+    all_data__ts_codes = result.fetchall()
+    print(all_data__ts_codes)
+    print('--------- end of select distinct ts_code from daily limit 10000:==========')
 
-    all_data_format = np.array(all_data)
+    sql_text = text(f''' select * from daily where ts_code=\'{all_data__ts_codes[0][0]}\' ''')
+    print(sql_text)
 
-    print('length of result:', len(all_data))
+    for ts_code in all_data__ts_codes:
+        print("+"*10, f'''begin analyze: {ts_code[0]}''', "+"*10)
+        sql_text = text(f''' select * from daily where ts_code=\'{ts_code[0]}\' limit 10000''')
+        result = conn.execute(sql_text)
 
-    print(all_data[1])
+        print('========== select * from daily result:==========')
+        all_data = result.fetchall()
+        print(all_data)
 
-    print(all_data[1][2])
+        all_data_format = np.array(all_data)
 
-    print(all_data_format)
+        print('length of result:', len(all_data))
 
-    print(all_data_format[1])
-    print(all_data_format[1][1])
+        print(all_data[1])
+
+        print(all_data[1][2])
+
+        print(all_data_format)
+
+        print(all_data_format[1])
+        print(all_data_format[1][1])
 
 
-    print('-------------------')
+        print('-------------------')
 
-    print('========== dataframe :==========')
+        print('========== dataframe :==========')
 
-    # dataframe的用法：https: // blog.csdn.net / Parzival_ / article / details / 114240650
+        # dataframe的用法：https: // blog.csdn.net / Parzival_ / article / details / 114240650
 
-    df_daily = pd.DataFrame(list(all_data))
-    print(df_daily)
-    print(df_daily.columns)
+        df_daily = pd.DataFrame(list(all_data))
+        print(df_daily)
+        print(df_daily.columns)
 
-    print("===iter elments in df_daily:===")
-    i = 0
-    # curIndex = 0
-    total_profit = 0
-    for row_index, row_value in df_daily.iterrows():
-        # if( row_value[8] < -9.5 ):
-        if (row_value[8] > 5 and row_value[8] < 9):
-            i = i + 1
-            print("+" * 5, f''' this is {i}td elements.''')
-            print(f'''    trade_date is {row_value['trade_date']}''')
-            print(f'''    row_index is {row_index} ; row_value is:\n{row_value} ''')
-            if row_index < len(df_daily) - 2 :
-                print(f''' the next daily is:\n{df_daily.iloc[row_index+1]}''')
-                print(f'''   buy:{row_value['close']} sell:{df_daily.iloc[row_index+1]['open']}  ''')
-                print(f'''   profit: {df_daily.iloc[row_index + 1]['open'] - row_value['close']}''')
-                total_profit += df_daily.iloc[row_index + 1]['open'] - row_value['close']
-            print("-" * 5, f''' end of this is {i}td elements.\n''')
-        #     print(f'curIndex = {curIndex}')
-        #     print(f'''curIndex = {curIndex} , the element is: {df_daily[curIndex]['trade_date']}''')
-        # curIndex += 1
-    print('-----end iter elements in df_daily')
-    print(f'''total_profit is {total_profit}\n''')
+        print("===iter elments in df_daily:===")
+        i = 0
+        # curIndex = 0
+        total_profit = 0
+        for row_index, row_value in df_daily.iterrows():
+            # if( row_value[8] < -9.5 ):
+            if (row_value[8] > 5 and row_value[8] < 9):
+                i = i + 1
+                print("+" * 5, f''' this is {i}td elements.''')
+                print(f'''    trade_date is {row_value['trade_date']}''')
+                print(f'''    row_index is {row_index} ; row_value is:\n{row_value} ''')
+                if row_index < len(df_daily) - 2 :
+                    print(f''' the next daily is:\n{df_daily.iloc[row_index+1]}''')
+                    print(f'''   buy:{row_value['close']} sell:{df_daily.iloc[row_index+1]['open']}  ''')
+                    print(f'''   profit: {df_daily.iloc[row_index + 1]['open'] - row_value['close']}''')
+                    total_profit += df_daily.iloc[row_index + 1]['open'] - row_value['close']
+                print("-" * 5, f''' end of this is {i}td elements.\n''')
+            #     print(f'curIndex = {curIndex}')
+            #     print(f'''curIndex = {curIndex} , the element is: {df_daily[curIndex]['trade_date']}''')
+            # curIndex += 1
+        print('-----end iter elements in df_daily')
+        print(f'''total_profit is {total_profit}\n''')
 
-    del df_daily['amount']
-    del df_daily['pct_chg']
-    del df_daily['_change']
-    del df_daily['pre_close']
-    del df_daily['ts_code']
+        del df_daily['amount']
+        del df_daily['pct_chg']
+        del df_daily['_change']
+        del df_daily['pre_close']
+        del df_daily['ts_code']
 
-    df_daily.index = pd.DatetimeIndex(df_daily['trade_date'])
-    del df_daily['trade_date']
+        df_daily.index = pd.DatetimeIndex(df_daily['trade_date'])
+        del df_daily['trade_date']
 
-    df_daily.columns=['open', 'high', 'low', 'close', 'volume']
+        df_daily.columns=['open', 'high', 'low', 'close', 'volume']
 
-    print(df_daily.columns)
-    print(df_daily)
+        # print(df_daily.columns)
+        # print(df_daily)
 
-    mpl.plot(df_daily, type='line', mav=(3, 6, 9), volume=True, axtitle='000001.sz')
+        mpl.plot(df_daily, type='line', mav=(3, 6, 9), volume=True, axtitle=ts_code[0])
 
-    # mpl.plot(df_daily, type='candle',mav=(3,6,9), volume=True)
+        # mpl.plot(df_daily, type='candle',mav=(3,6,9), volume=True)
 
-    print('-------------------')
+        print('-------------------')
+
+        print("-" * 10, f'''end of analyze: {ts_code[0]}''', "-" * 10, "\n\n")
+
+
+
+
+
+
 
 except Exception as e:
     print("\033[0;31;40m", e, "\033[0m")
