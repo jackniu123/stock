6075b7cb76d56f8924e8ec81d938a1af5a7eda61
jackniu123 <358137072@qq.com>
2024-05-28 17:03:24 +0800

    1，akshare在找不到增发日历时，是抛出了一个中文异常。

--- 工具软件/stock_assistant_add.py
+++ 工具软件/stock_assistant_add.py
@@ -19,7 +19,13 @@ def stock_assistant_add_main():
                 if (row['公告日期'] - datetime.datetime.now().date()).days >= 0:
                     print("!!! 这支股票要增发，妈的。")
                     messagebox.showerror('警告', f"{{{value[1]}}} 增发日期是 {{{row['公告日期']}}}")
-        except Exception as e:
+        except TypeError as te:
+            te_msg = f'{traceback.format_exc()}'
+            if '无增发' in te_msg:
+                print('found TypeError: \n', f'{traceback.format_exc()}')
+            else:
+                messagebox.showerror('出错了', te_msg)
+        except:
             messagebox.showerror('出错了', f'{traceback.format_exc()}')
 
     messagebox.logger.warning('===finished.')
