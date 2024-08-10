3246ad1712e7e63148f20ea75c07b0fa4907483f
jackniu123 <358137072@qq.com>
2024-08-10 15:56:39 +0800

    1，备份数据挖掘和监测苹果市场的资料

---
+++ 工具软件/stock_assistant_inspect_app_store.py
@@ -0,0 +1,2 @@
+# https://zh-cn.crawlbase.com/blog/apple-app-store-scraper/
+# https://app.diandian.com/rank/compete-1-4-0-75-4?time=1677600000000-1718899199000&timetype=custom&device=1
---
+++ 量价策略/数据挖掘/study_data_mining.py
@@ -0,0 +1,2 @@
+# https://blog.csdn.net/qazwsxpy/article/details/128192090
+# 二手房的影响因子探究：https://www.heywhale.com/mw/project/5f098536192ac2002c87c5aa
